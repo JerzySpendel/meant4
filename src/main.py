813286522daa7fde881a0cc11503913f91416a91
Jerzy Spendel <jspendel@gmail.com>
2024-02-24 21:56:02 +0100

    Add: 1. Images server 2. Actual broadcast implementation 3. Broadcast background task

--- src/main.py
+++ src/main.py
@@ -1,19 +1,80 @@
+import asyncio
+import contextlib
 from io import BytesIO
 import typing
-from fastapi import FastAPI, UploadFile, BackgroundTasks
+import fastapi
+from fastapi import FastAPI, UploadFile, BackgroundTasks, WebSocket, Request
 from fastapi.exceptions import HTTPException
+from fastapi.responses import FileResponse
 from PIL import Image, UnidentifiedImageError
 
-from src.processing import process_image
+from src.processing import image_task
+from src.queue import IMAGE_QUEUE
+from src.settings import BASE_PATH
 
-app = FastAPI()
+
+
+class WSManager:
+    def __init__(self, queue: asyncio.Queue):
+        self.connections: list[WebSocket] = []
+        self.queue = queue
+
+    async def connect(self, connection: WebSocket):
+        await connection.accept()
+        self.connections.append(connection)
+
+    async def disconnect(self, connection):
+        self.connections.remove(connection)
+
+    async def disconnect_all(self):
+        for connection in self.connections:
+            await connection.close()
+    
+    async def broadcast(self):
+        while True:
+            message = await self.queue.get()
+
+            for connection in self.connections:
+                await connection.send_text(message)
+
+
+@contextlib.asynccontextmanager
+async def setup_app(app):
+    ws = WSManager(queue=IMAGE_QUEUE)
+    app.extra['wsmanager'] = ws
+    task_handler = asyncio.create_task(ws.broadcast())
+    yield
+
+    task_handler.cancel()
+    await ws.disconnect_all()
+
+
+app = FastAPI(lifespan=setup_app)
 
 @app.post('/image')
-async def image(file: UploadFile, background_tasks: BackgroundTasks):
+async def image(file: UploadFile, background_tasks: BackgroundTasks, request: Request):
     try:
-        Image.open(BytesIO(await file.read()))
+        image = Image.open(BytesIO(await file.read()))
+        background_tasks.add_task(image_task, image, request)
     except UnidentifiedImageError:
         raise HTTPException(status_code=400, detail={'error': 'Input file doesn\'t seem to be an image file'})
 
-    background_tasks.add_task(process_image, file)
     return {'size': file}
+
+
+@app.get('/image/{image_filename}')
+async def serve_image(image_filename: str):
+    return FileResponse(
+        path=BASE_PATH / 'images' / image_filename
+    )
+
+
+@app.websocket('/faces')
+async def faces(connection: WebSocket):
+    manager: WSManager = app.extra['wsmanager']
+    await manager.connect(connection)
+    try:
+        while True:
+            await connection.receive()
+    except fastapi.WebSocketDisconnect:
+        manager.disconnect(connection)
--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,34 @@
 import asyncio
-from fastapi import UploadFile
+import uuid
+
+import numpy as np
+from io import BytesIO
+from fastapi import UploadFile, Request
+from face_recognition import face_locations
+from PIL import Image, ImageDraw
 
 from src.queue import IMAGE_QUEUE
+from src.settings import BASE_PATH
 
 
-async def process_image(file: UploadFile):
-    await asyncio.sleep(1)
-    print('Done!')
+def url_for_file(image_filename: str, request: Request) -> str:
+    host = request.headers['Host']
+    return f"http://{host}/image/{image_filename}"
+
+
+async def image_task(image: Image, request: Request):
+    uuid_filename = f"{str(uuid.uuid4())}.png"
+    processed_image = process_image(image)
+    processed_image.save(BASE_PATH / 'images' / uuid_filename)
+
+    await IMAGE_QUEUE.put(url_for_file(uuid_filename, request))
+
+
+def process_image(image: Image) -> list:
+    for location in face_locations(np.array(image)):
+        top, right, bottom, left = location
+
+        draw = ImageDraw.Draw(image)
+        draw.rectangle([left, top, right, bottom])
+
+    return image
