562bdc291545661fd8a46ab84a9f560306994bf2
Jerzy Spendel <jspendel@gmail.com>
2024-02-24 22:13:48 +0100

    Run black

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 import asyncio
 import contextlib
 from io import BytesIO
-import typing
-import fastapi
 from fastapi import FastAPI, UploadFile, BackgroundTasks, WebSocket, Request
 from fastapi.exceptions import HTTPException
 from fastapi.responses import FileResponse
@@ -11,7 +9,6 @@ from PIL import Image, UnidentifiedImageError
 from src.processing import image_task
 from src.queue import IMAGE_QUEUE
 from src.settings import BASE_PATH
-
 
 
 class WSManager:
@@ -38,7 +35,7 @@ class WSManager:
 @contextlib.asynccontextmanager
 async def setup_app(app):
     ws = WSManager(queue=IMAGE_QUEUE)
-    app.extra['wsmanager'] = ws
+    app.extra["wsmanager"] = ws
     task_handler = asyncio.create_task(ws.broadcast())
     yield
 
@@ -47,25 +44,27 @@ async def setup_app(app):
 
 app = FastAPI(lifespan=setup_app)
 
-@app.post('/image')
+
+@app.post("/image")
 async def image(file: UploadFile, background_tasks: BackgroundTasks, request: Request):
     try:
         image = Image.open(BytesIO(await file.read()))
         background_tasks.add_task(image_task, image, request)
     except UnidentifiedImageError:
-        raise HTTPException(status_code=400, detail={'error': 'Input file doesn\'t seem to be an image file'})
+        raise HTTPException(
+            status_code=400,
+            detail={"error": "Input file doesn't seem to be an image file"},
+        )
 
-    return {'size': file}
+    return {"size": file}
 
 
-@app.get('/image/{image_filename}')
+@app.get("/image/{image_filename}")
 async def serve_image(image_filename: str):
-    return FileResponse(
-        path=BASE_PATH / 'images' / image_filename
-    )
+    return FileResponse(path=BASE_PATH / "images" / image_filename)
 
 
-@app.websocket('/faces')
+@app.websocket("/faces")
 async def faces(connection: WebSocket):
-    manager: WSManager = app.extra['wsmanager']
+    manager: WSManager = app.extra["wsmanager"]
     await manager.connect(connection)
--- src/processing.py
+++ src/processing.py
@@ -12,14 +12,14 @@ from src.settings import BASE_PATH
 
 
 def url_for_file(image_filename: str, request: Request) -> str:
-    host = request.headers['Host']
+    host = request.headers["Host"]
     return f"http://{host}/image/{image_filename}"
 
 
 async def image_task(image: Image, request: Request):
     uuid_filename = f"{str(uuid.uuid4())}.png"
     processed_image = process_image(image)
-    processed_image.save(BASE_PATH / 'images' / uuid_filename)
+    processed_image.save(BASE_PATH / "images" / uuid_filename)
 
     await IMAGE_QUEUE.put(url_for_file(uuid_filename, request))
 
