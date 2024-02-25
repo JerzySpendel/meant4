2fdadfb419424c04be56d78b6b86714af377da1e
Jerzy Spendel <jspendel@gmail.com>
2024-02-25 20:43:03 +0100

    Add a few tests testing the HTTP API

---
+++ src/tests/test_api.py
@@ -0,0 +1,47 @@
+from io import BytesIO
+import pytest
+
+from PIL import Image
+from fastapi.testclient import TestClient
+
+from src.settings import BASE_PATH
+from src.main import app as app_object
+
+
+@pytest.fixture()
+def app() -> TestClient:
+    return TestClient(app_object)
+
+
+@pytest.fixture()
+def sample_file() -> str:
+    file = BASE_PATH / 'images' / 'sample.jpg'
+    file.touch()
+    yield file.name
+    file.unlink()
+
+
+@pytest.fixture()
+def sample_image() -> bytes:
+    path = BASE_PATH / 'src' / 'tests' / 'sample.jpg'
+    return path.read_bytes()
+
+
+def test_getting_existing_image(app, sample_file):
+    response = app.get(f"/image/{sample_file}")
+    assert response.status_code == 200
+
+
+def test_getting_not_existing_image(app):
+    response = app.get(f"/image/iamnothere.png")
+    assert response.status_code == 404
+
+
+def test_submitting_incorrect_image(app):
+    response = app.post('/image', files={'file': BytesIO(b'asdf')})
+    assert response.status_code == 400
+
+
+def test_submitting_correct_image(app, sample_image):
+    response = app.post('/image', files={'file': sample_image})
+    assert response.status_code == 200
